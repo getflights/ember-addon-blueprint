eab2b6bfad17e40a84821f2d612598ab861cb57e
BoussonKarel <bousson.karel@gmail.com>
2025-04-03 15:10:38 +0200

    remove excess whitespace (prettier error)

--- files/eslint.config.mjs
+++ files/eslint.config.mjs
@@ -27,9 +27,9 @@ const esmParserOptions = {
 };
 <% if (typescript) { %>
 const tsParserOptions = {
-    projectService: true,
-    project: true,
-    tsconfigRootDir: import.meta.dirname,
+  projectService: true,
+  project: true,
+  tsconfigRootDir: import.meta.dirname,
 };
 <% } %>
 const config = [
