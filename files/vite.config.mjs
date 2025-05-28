ef70869d7869e2bb518d4bd6ae60f6664c6c0856
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-28 15:36:02 -0400

    Fix self-imports so that you no longer are required to perform a build in order to get them to work at all

--- files/vite.config.mjs
+++ files/vite.config.mjs
@@ -3,6 +3,14 @@ import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '<%= name %>',
+        replacement: `${__dirname}/src`,
+      },
+    ],
+  },
   plugins: [
     ember(),
     babel({
